ba3b6567e3df3199df747a207f34f8ec0e37589d
Brittanie Morgan <brittaniemorgan12345@gmail.com>
2024-04-06 15:24:13 -0500

    Created movie form

--- app/models.py
+++ app/models.py
@@ -1,1 +1,11 @@
-# Add any model classes for Flask-SQLAlchemy here
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextArea
+from wtforms.validators import InputRequired
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+
+
+class MovieForm(FlaskForm):
+    title = StringField('Username', validators=[InputRequired()])
+    description = TextArea('Password', validators=[InputRequired()])
+    poster = FileField('File', validators=[FileRequired(), FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+
